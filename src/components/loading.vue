9cd4496d6c40ea02e2f81ae10d0f31f602a476d0
1539027251@qq.com <1539027251@qq.com>
2018-11-30 17:30:43 +0800

    'update'

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,12 +1,12 @@
 <template>
   <div v-if="visible">
-    <div class="loading" v-if="visible === true">
+    <div class="loading-com loading" v-if="visible === true">
       <slot><i class="el-icon-loading"></i>数据加载中...</slot>
     </div>
-    <div class="nothing" v-else-if="visible === 'nothing'">
+    <div class="loading-com nothing" v-else-if="visible === 'nothing'">
       <slot name="nothing">没有符合条件的数据</slot>
     </div>
-    <div class="error" v-else-if="visible === 'error'">
+    <div class="loading-com error" v-else-if="visible === 'error'">
       <slot name="error"><i class="el-icon-error"></i>请求错误，点击重新加载</slot>
     </div>
   </div>
@@ -22,35 +22,24 @@ export default {
 </script>
 
 <style lang="stylus">
-.loading{
+.loading-com{
   width: 100%;
   height: 50px;
   line-height: 50px;
   text-align: center;
   font-size: 14px;
-  color: #60a3f5;
   i{
     margin-right: 5px;
   }
+}
+.loading{
+  color: #60a3f5;
 }
 .nothing{
   margin: 15px 0;
-  width: 100%;
-  height: 50px;
-  line-height: 50px;
-  text-align: center;
-  font-size: 14px;
   color: #999;
 }
 .error{
-  width: 100%;
-  height: 50px;
-  line-height: 50px;
-  text-align: center;
-  font-size: 14px;
   color: #ff4949;
-  i{
-    margin-right: 5px;
-  }
 }
 </style>
